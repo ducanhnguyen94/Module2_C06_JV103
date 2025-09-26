e734e0e968385eb6f02b6281ad28cf0d07f05173
ducanhnguyen94 <ndanh4198@gmail.com>
2025-09-26 20:53:50 +0700

    Module 2

---
+++ Lesson_2_Loop/src/com/codegym/GreatestCommonDividor/GreatestCommonDividor.java
@@ -0,0 +1,28 @@
+package com.codegym.GreatestCommonDividor;
+
+import java.util.Scanner;
+
+public class GreatestCommonDividor {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int a, b, min, gcd = 1;
+
+        System.out.println("Enter a: ");
+        a = scanner.nextInt();
+
+        System.out.println("Enter b: ");
+        b = scanner.nextInt();
+
+        if (a >= b) min = b;
+        else min = a;
+
+        for (int i = 1; i <= min; i++) {
+            if (a % i == 0 && b % i == 0) {
+                if (i > gcd) gcd = i;
+            }
+        }
+
+        System.out.println("GCD is : " + gcd);
+    }
+}
