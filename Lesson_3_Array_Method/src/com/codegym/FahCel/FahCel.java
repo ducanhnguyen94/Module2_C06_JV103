940266a54ff379258c542dfc6187f2ee431768a5
ducanhnguyen94 <ndanh4198@gmail.com>
2025-09-27 11:33:52 +0700

    Module 2

---
+++ Lesson_3_Array_Method/src/com/codegym/FahCel/FahCel.java
@@ -0,0 +1,39 @@
+package com.codegym.FahCel;
+
+import java.util.Scanner;
+
+public class FahCel {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int choice;
+        double Cel, Fah;
+        do {
+            System.out.println("1. F to C");
+            System.out.println("2. C to F");
+            System.out.println("0. Exit");
+            System.out.println("YOUR OPTION: ");
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter F:");
+                    Fah = scanner.nextDouble();
+                    Cel = (Fah - 32) * 5 / 9;
+                    System.out.println("C: " + Cel);
+                    break;
+                case 2:
+                    System.out.println("Enter C: ");
+                    Cel = scanner.nextDouble();
+                    Fah = Cel * 9 / 5 + 32;
+                    System.out.println("F: " + Fah);
+                    break;
+                case 0:
+                    System.out.println("EXIT");
+                    break;
+                default:
+                    System.out.println("Not an option! Please try again");
+                    break;
+            }
+        } while (choice != 0);
+    }
+}
