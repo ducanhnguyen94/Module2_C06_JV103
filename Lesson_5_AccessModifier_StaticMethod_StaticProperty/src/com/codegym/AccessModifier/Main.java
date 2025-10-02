ad3f008ffb44e8246e4694f6d86dcf28cd122106
ducanhnguyen94 <ndanh4198@gmail.com>
2025-10-02 12:03:59 +0700

    Module 2

--- Lesson_5_AccessModifier_StaticMethod_StaticProperty/src/com/codegym/AccessModifier/Main.java
+++ Lesson_5_AccessModifier_StaticMethod_StaticProperty/src/com/codegym/AccessModifier/Main.java
@@ -10,5 +10,6 @@ public class Main {
 
         System.out.println("Radius of circle: " + radius);
         circle.getArea();
+        
     }
 }
---
+++ Lesson_5_AccessModifier_StaticMethod_StaticProperty/src/com/codegym/Student/Main.java
@@ -0,0 +1,12 @@
+package com.codegym.Student;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student  = new Student();
+
+        student.setName("Nguyen Duc Anh");
+        student.setClasses("C04");
+
+        System.out.println(student.toString());
+    }
+}
