ee8e0e7c5c88eaba7c6410de10ebe1b780db9d20
ducanhnguyen94 <ndanh4198@gmail.com>
2025-10-02 13:59:39 +0700

    Module 2

---
+++ Lesson_6_Inherritance/src/com/codegym/Shape/Main.java
@@ -0,0 +1,9 @@
+package com.codegym.Shape;
+
+public class Main {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle(3,2,3);
+        System.out.println(triangle.getArea());
+        System.out.println(triangle.toString());
+    }
+}
---
+++ Lesson_6_Inherritance/src/com/codegym/Shape/Triangle.java
@@ -0,0 +1,22 @@
+package com.codegym.Shape;
+
+public class Triangle extends Shape {
+
+    public Triangle() {
+        super();
+    }
+
+    public Triangle(double side1, double side2, double side3) {
+        super(side1, side2, side3, 0);
+    }
+
+    public double getPerimeter() {
+        return super.getSide1() + super.getSide2() + super.getSide3();
+    }
+
+    public double getArea() {
+        double peri = this.getPerimeter() / 2;
+        double area = Math.sqrt(peri*(peri - super.getSide1())*(peri - super.getSide2())*(peri - super.getSide3()));
+        return area;
+    }
+}
