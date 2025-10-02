56d44541aaf26bf8e371b07d64ac3b0967cc8573
ducanhnguyen94 <ndanh4198@gmail.com>
2025-10-02 12:45:11 +0700

    Module 2

---
+++ Lesson_6_Inherritance/src/com/codegym/Point2DPoint3D/Point2D.java
@@ -0,0 +1,46 @@
+package com.codegym.Point2DPoint3D;
+
+public class Point2D {
+    private float x, y;
+
+    public Point2D() {
+        this.x = 0.0f;
+        this.y = 0.0f;
+    }
+
+    public Point2D(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float[] getXY() {
+        float[] arr = {this.x, this.y};
+        return arr;
+    }
+
+    public void setXY(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point2D [x=" + x + ", y=" + y + "]";
+    }
+}
---
+++ Lesson_6_Inherritance/src/com/codegym/Point2DPoint3D/Point3D.java
@@ -0,0 +1,42 @@
+package com.codegym.Point2DPoint3D;
+
+public class Point3D extends Point2D {
+    private float z;
+
+    public Point3D(float z) {
+        this.z = z;
+    }
+
+    public Point3D(float x, float y, float z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public Point3D() {
+        super();
+        this.z = 0.0f;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+
+    public float[] getXYZ() {
+        float[] arr = {super.getX(), super.getY(), this.z};
+        return arr;
+    }
+
+    public void setXYZ(float x, float y, float z) {
+        super.setXY(x, y);
+        this.z = z;
+    }
+
+    @Override
+    public String toString() {
+        return "Point3D [z=" + z + "]" + " is a sublcass of " + super.toString();
+    }
+}
