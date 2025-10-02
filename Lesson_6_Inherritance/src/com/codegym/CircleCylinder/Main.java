12144fafbd7965c05bca709aa1e0c4b184c2e4e5
ducanhnguyen94 <ndanh4198@gmail.com>
2025-10-02 12:36:39 +0700

    Module 2

---
+++ Lesson_6_Inherritance/src/com/codegym/CircleCylinder/Main.java
@@ -0,0 +1,9 @@
+package com.codegym.CircleCylinder;
+
+public class Main {
+    public static void main(String[] args) {
+        Cylinder cylinder = new Cylinder(10);
+        System.out.println(cylinder.getVolumn());
+        System.out.println(cylinder.toString());
+    }
+}
