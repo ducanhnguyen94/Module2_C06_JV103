affe931ac7371a61e39416fed13e45ea3608b196
ducanhnguyen94 <ndanh4198@gmail.com>
2025-10-05 12:53:43 +0700

    Module 2

---
+++ Lesson_7_Abstract_Interface/src/com/codegym/Comparator/Circle.java
@@ -0,0 +1,45 @@
+package com.codegym.Comparator;
+
+public class Circle extends Shape {
+    private double radius;
+
+    public Circle() {
+        this.radius = 1.0;
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(String color, boolean filled, double radius) {
+        super(color, filled);
+        this.radius = radius;
+    }
+ 
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.pow(this.radius, 2) * Math.PI;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return (this.radius * 2 * Math.PI);
+    }
+
+    @Override
+    public String toString() {
+        return "A circle with radius=" + this.radius + ", which is a subclass of " + super.toString();
+    }
+
+
+
+    
+}
---
+++ Lesson_7_Abstract_Interface/src/com/codegym/Comparator/Rectangle.java
@@ -0,0 +1,37 @@
+package com.codegym.Comparator;
+
+import com.codegym.Comparable.Shape.Shape;
+
+public class Rectangle extends Shape{
+    private double width, length;
+
+    public Rectangle() {}
+
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
+
+    public Rectangle(String color, boolean filled, double width, double length) {
+        super(color, filled);
+        this.width = width;
+        this.length = length;
+    }
+
+    @Override
+    public double getArea() {
+        return this.width * this.length;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return (this.width + this.length) * 2;
+    }
+
+    @Override
+    public String toString() {
+        return "A Rectangle with width==" + this.width + " and length=" + this.length + ", which is a subclass of " + super.toString();
+    }
+
+    
+}
